b9677d1ff69513efea0c2cc8bec81a497628e5f7
wratterman <will.ratterman@gmail.com>
2017-08-30 09:29:17 -0600

    converted user story to first version of rspec test. Now to build out the functionalities

--- spec/features/user_can_search_by_zip_spec.rb
+++ spec/features/user_can_search_by_zip_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
-# As a user
-# When I visit "/"
-# And I fill in the search form with 80203
-# And I click "Locate"
-# Then I should be on page "/search" with parameters visible in the url
-# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-# And the stations should be limited to Electric and Propane
-# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+describe "User Can Search By Zip" do
+  it "user visits root page and searches for stations by zip" do
+
+    visit "/"
+    fill_in["search[body]", with: "80203"]
+    click_on "Locate"
+
+    expect(current_path).to eq("/search?80203")
+    expect(page).to have_content("Wells Fargo Center")
+    expect(page).to have_content("1700 Sherman Street")
+    expect(page).to have_content("Electric")
+    # expect(page).to have_content("distance")
+    expect(page).to have_content("Open 24/7")
+  end
+end
