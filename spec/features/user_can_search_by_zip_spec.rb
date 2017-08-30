ffef2b042d772e362dac30e29dd0e22f3b43a816
wratterman <will.ratterman@gmail.com>
2017-08-30 09:16:39 -0600

    setup first user story test and ENV[AFS_KEY]

---
+++ spec/features/user_can_search_by_zip_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+# As a user
+# When I visit "/"
+# And I fill in the search form with 80203
+# And I click "Locate"
+# Then I should be on page "/search" with parameters visible in the url
+# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+# And the stations should be limited to Electric and Propane
+# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
